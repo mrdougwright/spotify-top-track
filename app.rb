600a40a73edc55221c4d2fcbd47a228c6f4766c1
Doug Wright <mrdougwright@gmail.com>
2017-03-06 21:35:54 -0800

    add style and meta data

--- app.rb
+++ app.rb
@@ -10,14 +10,14 @@ get '/' do
   erb :index
 end
 
-post '/artist' do
-  @artist = get_artist_and_track(params[:name])
+route :get, :post '/artist' do
+  @artist = get_artist_info(params[:name])
   erb :artist
 end
 
 post '/send_text' do
   content_type :json
-  artist = get_artist_and_track(params[:name])
+  artist = get_artist_info(params[:name])
   @msg_sent = send_text_message(artist[:name], artist[:track], params[:phone])
   { msg_sent: @msg_sent, artist: artist }.to_json
 end
--- lib/helpers/artist_info.rb
+++ lib/helpers/artist_info.rb
@@ -1,11 +1,29 @@
 module ArtistInfo
-  def get_artist_and_track(name)
+  def get_artist_info(name)
     s = SpotifyApi.new
     artist = s.find_artist(name)
     track  = s.find_track(artist)
+    decorate_artist_data(artist, track)
+  end
+
+  def get_image_url(artist)
+    images = artist&.images
+    img = images&.select do |i|
+      i['height'] >= 300 && i['height'] <= 400
+    end.first
+    img ? img['url'] : images&.first['url']
+  end
+
+  def decorate_artist_data(artist, track)
     {
       name: artist&.name,
-      track: track&.name
+      track: track&.name,
+      image_url: get_image_url(artist),
+      popularity: artist&.popularity,
+      genres: artist&.genres,
+      followers: artist&.followers['total'],
+      related: artist&.related_artists.map(&:name),
+      spotify_link: artist&.external_urls['spotify']
     }
   end
 end
